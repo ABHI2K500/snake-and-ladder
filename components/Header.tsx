aae5c73ba7f341c8a86fdcb2e6b151bad1182b9d
ABHIJITH P <183481970+ABHI2K500@users.noreply.github.com>
2025-09-17 23:42:59 +0530

    Add files via upload

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header className="text-center">
       <h1 className="text-5xl md:text-7xl font-black tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-emerald-400 via-sky-300 to-rose-500">
-        Tech Snakes & Ladders
+        Brain Board
       </h1>
       <p className="mt-4 max-w-3xl mx-auto text-lg text-gray-300">
         Welcome to the tech challenge! When you land on a ladder, complete a task to climb.
